19e6597017e26c460449535a3c1e6260115be144
UncleJerry <jared.myhrberg@gmail.com>
2019-04-03 16:20:59 -0700

    updated after review with ryan

--- test/jsonFs.test.js
+++ test/jsonFs.test.js
@@ -5,14 +5,16 @@ describe('jsonFs', () => {
 
   it('json read function', done => {
     const json = JSON.stringify({ result: true, count: 42 });
-    fs.writeFile('./json-file.txt', json, { encoding: 'utf8' }, (err) => {
-      readJSON('./json-file.txt', data => {
-        expect(data).toEqual({ result: true, count: 42 });
+    fs.writeFile('./json-file.txt', json, { encoding: 'utf8' }, () => {
+      readJSON('./json-file.txt', (err, obj) => {
+        expect(err).toBeFalsy();
+        expect(obj).toEqual({ result: true, count: 42 });
         done();
       });
+
     });
   });
-
+  
   it('json write funciton', done => {
     writeJSON('./json-write-file.txt', { result: true, count: 42 }, () => {
       fs.readFile('./json-write-file.txt', { encoding: 'utf8' }, (err, data) => {
