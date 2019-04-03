d76a23dc09430da56ebd70b25107cd7189bf0d8f
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-03 14:56:02 -0700

    copy file function working and test are passing

--- test/copy.test.js
+++ test/copy.test.js
@@ -1,14 +1,18 @@
-const { fs } = require('fs');
-
+const { copy } = require('../lib/copy.js');
+const fs = require('fs');
 describe('copy file function', () => {
-    afterEach(() => {
-        fs.unlink;
-    });
-    it('use copy file logic to creat reusable function', () => {
-        const result = copy('../writing.txt', '../copy-file.txt', (err) => {
-            if(err) throw err;
-            
+    it('use copy file logic to creat reusable function', done => {
+        afterEach(done => {
+            fs.unlink('./copy-file.txt', done);
         });
-        expect(result).toEqual();
+        copy('./writing.txt', './copy-file.txt', (err) => {
+            expect(err).toBeUndefined();
+            fs.readFile('./writing.txt', { encoding: 'utf8' }, (err, original) => {
+                fs.readFile('./copy-file.txt', { encoding: 'utf8' }, (err, copiedFile) => {
+                    expect(original).toEqual(copiedFile);
+                    done();
+                });
+            });
+        });
     });
 });
