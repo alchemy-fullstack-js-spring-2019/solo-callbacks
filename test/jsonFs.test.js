88fddd731b63ebe7cfc1f98838e460361ae8674a
Sean Nelson <smnelsonn@gmail.com>
2019-04-03 16:09:04 -0700

    done

--- test/jsonFs.test.js
+++ test/jsonFs.test.js
@@ -2,7 +2,7 @@ const { readJSON, writeJSON } = require('../lib/jsonFs.js');
 const fs = require('fs');
 
 describe('read a json', () => {
-  it('test readJson', () => {
+  it('test readJson', done => {
     const obj = { message: 'hi' };
     const json = JSON.stringify(obj);
     fs.writeFile('./jsonTest.txt', json, (err) => {
@@ -10,16 +10,18 @@ describe('read a json', () => {
       readJSON('./jsonTest.txt', (err, data) => {
         if(err) throw err;
         expect(data).toEqual(JSON.parse(json));
+        done();
       });
     });
   });
   
-  it('test writeJson', () => {
+  it('test writeJson', done => {
     const obj = { message: 'hi' };
     writeJSON('./jsonWriteTest.txt', obj, () => {
       readJSON('./jsonWriteTest.txt', (err, data) => {
         if(err) throw err;
         expect(data).toEqual(obj);
+        done();
       });
     });
   });
