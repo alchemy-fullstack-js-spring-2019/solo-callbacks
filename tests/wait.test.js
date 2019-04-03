2a22859eb4902033b6251057e08d388feb0b084b
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-03 10:56:29 -0700

    wait refactoring

--- tests/wait.test.js
+++ tests/wait.test.js
@@ -2,8 +2,7 @@ const wait = require('../lib/wait');
 
 describe('wait function', () => {
     it('waits 1 second then invokes callback', done => {
-        wait(1000, () => {
-            done();
-        });
+        wait(1000, done);
     });
 });
+
