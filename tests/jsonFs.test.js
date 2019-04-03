b81db839873ad9801edae8aae48f9872a2c9f4ef
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-03 16:09:01 -0700

    revised test

--- tests/jsonFs.test.js
+++ tests/jsonFs.test.js
@@ -3,15 +3,17 @@ const fs = require('fs');
 
 describe('read json function', () => {
     it('reads the json file', done => {
-        const filePath = '../package.json';
-        readJSON(filePath, err => {
-            expect(err).toBeFalsy();
-            fs.writeFile(filePath, './newfile.txt', err => {
-                readJSON('./newfile.txt', err => {
-                    expect(filePath).toEqual('./newfile.txt');
-                    done();
-                });
-            });
+        const dog = {
+            name: 'spot',
+            age: 10
+        };
+
+        fs.writeFile('./myJSON', JSON.stringify(dog), () => {
+            readJSON('./myJSON', (err, data) => {
+                expect(err).toBeFalsy();
+                expect(data).toEqual(dog);
+                done();
+            })
         });
             
         //use writeFile to write string to file
