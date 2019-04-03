88f3c5ca7b08faababb96c82ea447cfc9a800b37
Payton Lower <paytown@me.com>
2019-04-03 16:03:02 -0700

    work in progress JSON before lecture

---
+++ test/jsonfs.test.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const { readJSON } = require('../lib/jsonfs');
+
+describe('json', () => {
+
+
+  it('writes and reads JSON', done => {
+    const json = JSON.stringify({ name: 'Timmothy', age: 23 });
+    fs.writeFile('./json.txt', json, 'utf8', () => {
+      readJSON('./json.txt', data => {
+        expect(data).toEqual({ name: 'Timmothy', age: 23 });
+        done();
+      });
+    });
+  });
+});
