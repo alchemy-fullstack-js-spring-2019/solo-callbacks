b74e961f5a869bb015f070177267b148968b0cf9
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-03 13:47:25 -0700

    copyFile test passing

--- tests/copy.test.js
+++ tests/copy.test.js
@@ -1,9 +1,16 @@
 const copy = require('../lib/copyFile');
+const fs = require('fs');
+const { readFile } = require('../lib/readFile.js');
 
 describe('copy function', () => {
-    it('copies a file', () => {
-        const expected = '../copy.txt';
-        const result = copy('../1_callbacks.md', '../copy.txt', callback);
-        expect(result.equals(expected)).toBe(true);
+    it('copies a file and invokes a callback', done => {
+        copy('../writing.txt', '../writing3.txt', err => {
+            expect(err).toBeFalsy();
+        });
+        fs.readFile('../writing3.txt', 'utf8', (err, data) => {
+            expect(err).toBeFalsy();
+            expect(data).toEqual('I am writing!!!');
+        });
+        done();
     });
 });
