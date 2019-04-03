225f95ebeb6769e5716afbe9b7dd174fa82db104
Leslie Shea <leslieyshea@gmail.com>
2019-04-03 16:25:30 -0700

    wrote writeJSON function with passing test

--- test/jsonFs.test.js
+++ test/jsonFs.test.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
-const { readJSON } = require('../lib/jsonFs');
+const { readJSON, writeJSON } = require('../lib/jsonFs');
 
 describe('jsonFs function', () => {
+  afterEach(done => {
+    fs.unlink('./json.txt', done);
+  });
+
   it('can read a json file', done => {
     const json = {
       name: 'leslie',
@@ -11,7 +15,23 @@ describe('jsonFs function', () => {
     fs.writeFile('./json.txt', JSON.stringify(json), () => {
       readJSON('./json.txt', (err, data) => {
         expect(err).toBeFalsy();
-        expect(data).toEqual({ name: 'leslie', age: 25 });
+        expect(data).toEqual(json);
+        done();
+      });
+    });
+  });
+
+  it('can write a json file', done => {
+    const json = {
+      name: 'leslie',
+      age: 25
+    };
+
+    writeJSON('./json.txt', json, err => {
+      expect(err).toBeFalsy();
+
+      readJSON('./json.txt', (err, data) => {
+        expect(data).toEqual(json);
         done();
       });
     });
