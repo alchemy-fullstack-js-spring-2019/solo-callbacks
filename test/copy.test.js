f0db2651041c0ac5abc89f02798d22058cd1a092
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-03 14:45:44 -0700

    implements changes from Ryan's explanation

--- test/copy.test.js
+++ test/copy.test.js
@@ -4,19 +4,19 @@ const fs = require('fs');
 describe('copy function', () => {
   const src = './2_fs.md';
   const dst = './2_fs-copy.md';
-  afterEach(() => {
-    fs.unlink(dst, (err) => {
-      if(err) throw err;
-    });
+  afterEach(done => {
+    fs.unlink(dst, done);
   });
   it('copies the contents of a file to a new location', done => {
     copy(src, dst, (err) => {
       if(err) throw err;
       
-      fs.readFile(dst, { encoding: 'utf8' }, (err, data) => {
-        if(err) throw err;
-        expect(data).toEqual(expect.any(String));
-        done();
+      fs.readFile(src, { encoding: 'utf8' }, (err, original) => {
+        fs.readFile(dst, { encoding: 'utf8' }, (err, copied) => {
+          expect(original).toEqual(copied);
+          done();
+
+        });
       });
     });
 
