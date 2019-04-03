456d476364400b34541b491069227871055a47c0
mcnadams <mcneilbo@oregonstate.edu>
2019-04-03 10:43:55 -0700

    TDD wait function

--- tests/wait.test.js
+++ tests/wait.test.js
@@ -2,6 +2,7 @@ const wait = require('../lib/wait');
 
 describe('callbacks', () => {
     it('waits one second and invokes a callback', done => {
-        
+        wait(2000);
+        done();
     });
 });
