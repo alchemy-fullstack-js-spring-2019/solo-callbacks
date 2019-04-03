4dcd9cd08e42aae789c02857ec9d4c5405b61531
mcnadams <mcneilbo@oregonstate.edu>
2019-04-03 15:16:18 -0700

    readJSON and tests appear to work

--- tests/jsonFs.test.js
+++ tests/jsonFs.test.js
@@ -1,8 +1,40 @@
 const fs = require('fs');
 const { readJSON } = require('../lib/jsonFs');
 
+const sampleJSON = {
+    'glossary': {
+        'title': 'example glossary',
+        'GlossDiv': {
+            'title': 'S',
+            'GlossList': {
+                'GlossEntry': {
+                    'ID': 'SGML',
+                    'SortAs': 'SGML',
+                    'GlossTerm': 'Standard Generalized Markup Language',
+                    'Acronym': 'SGML',
+                    'Abbrev': 'ISO 8879:1986',
+                    'GlossDef': {
+                        'para': 'A meta-markup language, used to create markup languages such as DocBook.',
+                        'GlossSeeAlso': ['GML', 'XML']
+                    },
+                    'GlossSee': 'markup'
+                }
+            }
+        }
+    }
+};
+
+
 describe('readJSON tests', () => {
-    it('better work', () => {
-        
+    it('better work', done => {
+        const stringified = JSON.stringify(sampleJSON);
+        fs.writeFile('json-string.txt', stringified, err => {
+            if(err) throw err;
+            const result = readJSON('json-string.txt', (err, result) => {
+                if(err) throw err;
+                expect(result).toEqual(sampleJSON);
+                done();
+            });
+        });
     });
 });
