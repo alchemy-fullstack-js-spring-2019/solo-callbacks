920d9fe01e22af8cc0a63542bee003747a0d5a31
ollinieminen <otn987@gmail.com>
2019-04-03 14:49:07 -0700

    need to revisit copy.  its all broken!!! learn async!

--- copy.test.js
+++ copy.test.js
@@ -1,28 +1,40 @@
 const copyToLocation = require('./copy');
-const readFile = require ('./readFile');
+const readFile = require('./readFile');
+const fs = require('fs');
 
 //giving a bad destination to read from will crash node :(
-describe('COPY.JS**',()=>{
-    it('read a file from existing path and copy it to another location, throw error when path is wrong', done=>{
-        const readFrom = './1_callbacks.m';
-        const copyTo = './writing.txt';  
-        copyToLocation(readFrom,copyTo, (err)=>{
-            expect(err).toBeDefined();      
-            done();
-        });  
-      
-    })
-})
+describe('COPY.JS**', () => {
 
 
-describe('COPY.JS**',()=>{
-    it('check destination contains stuff from src', ()=>{
+    afterEach(done => {
+        fs.unlink('./writing.txt', (err) => {
+            done(err);
+        })
+    });
+
+    it('check destination contains stuff from src', done => {
         const readFrom = './1_callbacks.md';
-        const copyTo = './writing.txt';  
-        copyToLocation(readFrom, copyTo);
-        const sourceContent = readFile(readFrom);
-        const destinationContent = readFile(copyTo);
-        expect(sourceContent).toBe(destinationContent);
+        const copyTo = './writing.txt';
+        copyToLocation(readFrom, copyTo, ()=>{
+            const sourceContent = readFile(readFrom);
+            const destinationContent = readFile(copyTo);
+            console.log('3$^$$^$%#$%#', destinationContent);
+            expect(sourceContent).toBe(destinationContent);
+            done();
+        });
+    
+        
+    })
+
+    it('read a file from existing path and copy it to another location, throw error when path is wrong', done => {
+        const readFrom = './1_callbacks.m';
+        const copyTo = './writing.txt';
+        copyToLocation(readFrom, copyTo, (err) => {
+            expect(err).toBeDefined();
+            done();
+        });
+
     })
 })
- 
+
+
