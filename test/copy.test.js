653a921859fea7b4eff0697a61ba4426618a17d6
Payton Lower <paytown@me.com>
2019-04-03 11:58:07 -0700

    test passes

---
+++ test/copy.test.js
@@ -0,0 +1,12 @@
+const copy = require('../lib/copy');
+
+describe('copy a file', () => {
+  it('copies a file', () => {
+
+    const original = ('message.txt');
+    const copied = ('new.txt');
+
+    expect(copy(original)).toEqual(copy(copied));
+
+  });
+});
