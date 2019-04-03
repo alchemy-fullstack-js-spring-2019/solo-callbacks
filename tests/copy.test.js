1f014ee7c7b2118e6129a5b4093604d4fd0b08de
mcnadams <mcneilbo@oregonstate.edu>
2019-04-03 14:09:40 -0700

    copy function

--- tests/copy.test.js
+++ tests/copy.test.js
@@ -2,8 +2,12 @@ const fs = require('fs');
 const copy = require('../lib/copy');
 
 describe('copy file', () => {
+    afterEach(() => {
+        fs.unlink('copied.txt', () => {});
+    });
     it('copies a file from src to dest', done => {
-        copy('../2_fs.md', 'copied.txt', () => {
+        copy('../2_fs.md', 'copied.txt', err => {
+            if(err) throw err;
             done();
         });
     });
---
+++ tests/jsonFs.test.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const { readJSON } = require('../lib/jsonFs');
+
+describe('readJSON tests', () => {
+    it('better work', () => {
+        
+    });
+});
