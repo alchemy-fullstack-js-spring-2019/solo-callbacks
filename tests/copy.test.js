758191ed00b8e108cbda257d2727e027719368e5
mcnadams <mcneilbo@oregonstate.edu>
2019-04-03 15:03:41 -0700

    I think the copy test is correct now

--- tests/copy.test.js
+++ tests/copy.test.js
@@ -6,9 +6,14 @@ describe('copy file', () => {
         fs.unlink('copied.txt', () => {});
     });
     it('copies a file from src to dest', done => {
-        copy('../2_fs.md', 'copied.txt', err => {
-            if(err) throw err;
-            done();
+        copy('./2_fs.md', 'copied.txt', err => {
+            expect(err).toBeFalsy();
+            fs.readFile('./2_fs.md', { encoding: 'utf8' }, (err, original) => {
+                fs.readFile('copied.txt', { encoding: 'utf8' }, (err, copiedFile) => {
+                    expect(original).toEqual(copiedFile);
+                    done();
+                });
+            });
         });
     });
 });
