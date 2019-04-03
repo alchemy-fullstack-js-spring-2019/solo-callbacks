746e1cbda91434a2eac13890c662bdb60f5e7e60
UncleJerry <jared.myhrberg@gmail.com>
2019-04-03 16:00:24 -0700

    write json passing tests

--- test/jsonFs.test.js
+++ test/jsonFs.test.js
@@ -1,14 +1,22 @@
-const readJSON = require('../lib/jsonFs');
+const { readJSON, writeJSON } = require('../lib/jsonFs');
 const fs = require('fs');
 
 describe('jsonFs', () => {
 
-  it('json function', done => {
+  it('json read function', done => {
     const json = JSON.stringify({ result: true, count: 42 });
     fs.writeFile('./json-file.txt', json, { encoding: 'utf8' }, (err) => {
-      if(err) throw err;
       readJSON('./json-file.txt', data => {
         expect(data).toEqual({ result: true, count: 42 });
+        done();
+      });
+    });
+  });
+
+  it('json write funciton', done => {
+    writeJSON('./json-write-file.txt', { result: true, count: 42 }, () => {
+      fs.readFile('./json-write-file.txt', { encoding: 'utf8' }, (err, data) => {
+        expect(data).toEqual('{"result":true,"count":42}');
         done();
       });
     });
