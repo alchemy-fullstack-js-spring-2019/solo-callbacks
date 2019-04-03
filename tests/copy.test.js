fa49f33dba80d71313db8aa1586c039315c77769
Ben Parisot <me@benparisot.com>
2019-04-03 14:23:49 -0700

    write copy function and pass test

--- tests/copy.test.js
+++ tests/copy.test.js
@@ -2,5 +2,19 @@ const fs = require('fs');
 const copy = require('../lib/copy');
 
 describe('copy function', () => {
-    
+  afterEach((done) => {
+    fs.unlink('./oneMoreFile.txt', (err) => {
+      done();
+    });
+  });
+  it('copies a file', done => {
+    copy('./newFile.js', './oneMoreFile.txt', err => {
+      expect(err).toBeFalsy();
+      fs.readFile('./oneMoreFile.txt', 'utf8', (err, data) => {
+        expect(err).toBeFalsy();
+        expect(data).toBe('Hi');
+        done();
+      });
+    });
+  });
 });
