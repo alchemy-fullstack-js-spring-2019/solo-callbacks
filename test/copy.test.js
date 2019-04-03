2ed05947d8457c817acc6dfc424e109107968460
Payton Lower <paytown@me.com>
2019-04-03 12:04:32 -0700

    working through copy

--- test/copy.test.js
+++ test/copy.test.js
@@ -3,8 +3,8 @@ const copy = require('../lib/copy');
 describe('copy a file', () => {
   it('copies a file', () => {
 
-    const original = ('message.txt');
-    const copied = ('new.txt');
+    const original = ('./message.txt');
+    const copied = ('./new.txt');
 
     expect(copy(original)).toEqual(copy(copied));
 
