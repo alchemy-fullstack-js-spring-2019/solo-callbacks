c901501d6dc18c64160c5ec171573b2496dc32e3
heather <unphurlng@gmail.com>
2019-04-03 14:59:17 -0700

    copy test and function

---
+++ test/copy.test.js
@@ -0,0 +1,16 @@
+const copy = require('../lib/copy');
+const fs = require('fs');
+
+describe('Copy function', () => {
+  it('Copies a file', done => {
+    copy('./1_callbacks.md', './1_callbacks-copy.md', err => {
+      expect(err).toBeUndefined();
+      fs.readFile('./1_callbacks.md', 'utf8', (err, original) => {
+        fs.readFile('./1_callbacks-copy.md', 'utf8', (err, copiedFile) => {
+          expect(original).toEqual(copiedFile);
+          done();
+        });
+      });
+    });
+  });
+});
