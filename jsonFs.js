91318dd04f51e74f95df25f432c95b8beddabb45
Masmics <cherlocker3113@gmail.com>
2019-04-03 20:17:09 -0700

    travis adjustments

--- jsonFs.js
+++ jsonFs.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 
-function readJSON(src, callback) {
-  fs.readFile(src, { encoding: 'utf8' }, (err, data) => {
+function readJSON(path, callback) {
+  fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
     if(err) return callback(err);
+
     const json = JSON.parse(data);
     callback(null, json);
   });
 }
 
-function writeJSON(src, data, callback) {
+function writeJSON(path, data, callback) {
   const strData = JSON.stringify(data);
-  fs.writeFile(src, strData, callback);
+  fs.writeFile(path, strData, callback);
 }
 
 module.exports = {
