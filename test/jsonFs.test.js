da8a1c90eb6865a67f10a2b72869093db3ba4463
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-03 18:32:43 -0700

    fixed tests to pass

--- test/jsonFs.test.js
+++ test/jsonFs.test.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const { readJson, writeJson } = require('../lib/jsonFs.js');
 
 describe('readJson function', () => {
-    // afterEach(done =>{
-    //     fs.unlink('./crazy.json', done);
-    // });
+    afterEach(done =>{
+        fs.unlink('./crazy.json', done);
+    });
     it('takes JSON and returns file with its contents', done => {
         const json = { name: 'fun stuff' };
         const stringJson = JSON.stringify(json);
@@ -17,11 +17,13 @@ describe('readJson function', () => {
         });
     });
     it('can write a json file', done => {
-        const json = { name: 'yolo' };
-        writeJson('./writtenJson.json', err => {
+        const jsonData = { name: 'yolo' };
+
+        writeJson('./crazy.json', jsonData, err => {
             expect(err).toBeFalsy();
-            readJson('./writtenJson.json', data => {
-                expect(data).toEqual(json);
+
+            readJson('./crazy.json', (err, data) => {
+                expect(data).toEqual(jsonData);
                 done();
             });
         });
