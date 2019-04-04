20827f4b3e35a0e0ed029ae4044bdee9a7000b26
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-03 18:08:47 -0700

    still have failing tests for read/write json

---
+++ test/jsonFs.test.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { readJson, writeJson } = require('../lib/jsonFs.js');
+
+describe('readJson function', () => {
+    // afterEach(done =>{
+    //     fs.unlink('./crazy.json', done);
+    // });
+    it('takes JSON and returns file with its contents', done => {
+        const json = { name: 'fun stuff' };
+        const stringJson = JSON.stringify(json);
+        fs.writeFile('./crazy.json', stringJson, () => {
+            readJson('./crazy.json', (err, data) => {
+                expect(err).toBeFalsy;
+                expect(data).toEqual(json);
+                done();
+            });
+        });
+    });
+    it('can write a json file', done => {
+        const json = { name: 'yolo' };
+        writeJson('./writtenJson.json', err => {
+            expect(err).toBeFalsy();
+            readJson('./writtenJson.json', data => {
+                expect(data).toEqual(json);
+                done();
+            });
+        });
+    });
+});
