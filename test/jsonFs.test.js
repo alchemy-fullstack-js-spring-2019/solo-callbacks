2a8c43a3833ef7c84e5df119f7007c2d591ae37a
Peter Kotowski <bighoopee@gmail.com>
2019-04-04 08:41:04 -0700

    added travis

--- test/jsonFs.test.js
+++ test/jsonFs.test.js
@@ -1,13 +1,30 @@
 const fs = require('fs');
 
 const { readJson } = require('../lib/jsonFs'); 
-
-
-describe('write json to file', () => {
-  it('stringify json', () => {
-    fs.readFile('../package.json', { encoding: 'utf8' }, (err, data) => {
-      data.toString(data);
+ 
+describe('json functions', () => {
+  it('reads JSON', done => {
+    const thing = {
+      name: 'anything',
+      type: 'something'
+    };
+    fs.writeFile(',/things.', JSON.stringify(thing), () => {
+      readJson('./things', (err, data) => {
+        expect(err).toBeFalsy();
+        expect(data).ToEqual(thing);
+        done();
+      });
     });
-
+  
   });
+
 });
+
+// describe('write json to file', () => {
+//   it('stringify json', () => {
+//     fs.readFile('../package.json', { encoding: 'utf8' }, (err, data) => {
+//       data.toString(data);
+//     });
+
+//   });
+// });
