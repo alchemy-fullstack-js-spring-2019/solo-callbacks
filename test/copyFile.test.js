a2f4ff0bd952e65af82b9a95499d78f7985bac81
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-03 20:42:04 -0700

    done for the night

--- test/copyFile.test.js
+++ test/copyFile.test.js
@@ -1,11 +1,21 @@
 const copyFile = require('../lib/copyFile');
-
+const fs = require('fs');
 
 
 describe('reads a file and writes a file to a new location', () => {
-  it('can copy a file', done => {
-    copyFile('../1_callbacks.md', 'new-new-copy.txt', done);
+  afterEach(done => {
+    fs.unlink('../1_callbacks-copy.md', done);
   });
-
+  it('can copy a file', done => {
+    copyFile('../1_callbacks.md', '../1_callbacks-copy.md', err => {
+      expect(err).toBeFalsy();
+      fs.readFile('../1_callbacks.md', { encoding: 'utf-8' }, (err, original) => {
+        fs.readFile('../1_callbacks-copy.md', { encoding: 'utf-8' }, (err, copy) => {
+          expect(copy).toEqual(original);
+        });
+      });
+    });
+  });
 });
+
 
