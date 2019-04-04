e1067dd7ca9995b52727d51aebc9723cfe6a9ff3
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-03 20:22:54 -0700

    tests somehow pass

--- test/jsonFs.test.js
+++ test/jsonFs.test.js
@@ -1,8 +1,11 @@
-const readJSON = require('../lib/jsonFs');
+const { readJSON, writeJSON } = require('../lib/jsonFs');
 const fs = require('fs');
 
-describe('reads string from json', () => {
-  it('writes a json file to a string', done => {
+describe('reads writes json', () => {
+  afterEach(done => {
+    fs.unlink('./test.json', done);
+  });
+  it('reads a json file', done => {
     const object = { key1: 'value-1', key2: 'value-2' };
     const json = JSON.stringify(object);
     fs.writeFile('./test.json', json, { encoding: 'utf8' }, () => {
@@ -15,10 +18,11 @@ describe('reads string from json', () => {
   });
   it('writes a json file from an object', done => {
     const object = { key: 'value', key2: 'value2' };
-    writeJSON('./test.json', object, (err, writeData) => {
+    writeJSON('./test.json', object, err => {
       expect(err).toBeFalsy();
-      readJSON('./test.json', (err, readData) => {
-        expect(writeData).toEqual(readData);
+
+      readJSON('./test.json', (err, data) => {
+        expect(data).toEqual(object);
         done();
       });
     });
