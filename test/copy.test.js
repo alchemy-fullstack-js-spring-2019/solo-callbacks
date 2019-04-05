0c6cacdce210a61696b09f32bd97b3a836c5a2bc
Leslie Shea <leslieyshea@gmail.com>
2019-04-05 15:01:41 -0700

    removed pseudo code

--- test/copy.test.js
+++ test/copy.test.js
@@ -8,12 +8,9 @@ describe('copy function', () => {
 
   it('can copy a file', done => {
     copy('./2_fs.md', './2_fs-copy.md', err => {
-      // expect error to not happen
       expect(err).toBeUndefined();
-      // read the copied file and the original file
       fs.readFile('./2_fs.md', { encoding: 'utf8' }, (err, originalFile) => {
         fs.readFile('./2_fs-copy.md', { encoding: 'utf8' }, (err, copiedFile) => {
-          // test that the copied file and original file are the same
           expect(originalFile).toEqual(copiedFile);
           done();
         });
