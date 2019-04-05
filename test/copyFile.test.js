d5497c3432807f5e819a8f6b8e92525d5a71758a
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-04 20:07:46 -0700

    complete!

--- test/copyFile.test.js
+++ test/copyFile.test.js
@@ -3,15 +3,24 @@ const fs = require('fs');
 
 
 describe('reads a file and writes a file to a new location', () => {
+  
+  beforeEach(() => {
+    fs.writeFile('./test.txt', 'string', err => {
+      if(err) throw err;
+    });
+  });
   afterEach(done => {
-    fs.unlink('../1_callbacks-copy.md', done);
+    fs.unlink('./test.txt', () => {
+      fs.unlink('./test-copy.txt', done);
+    });
   });
   it('can copy a file', done => {
-    copyFile('../1_callbacks.md', '../1_callbacks-copy.md', err => {
+    copyFile('./test.txt', './test-copy.txt', (err, data) => {
       expect(err).toBeFalsy();
-      fs.readFile('../1_callbacks.md', { encoding: 'utf-8' }, (err, original) => {
-        fs.readFile('../1_callbacks-copy.md', { encoding: 'utf-8' }, (err, copy) => {
+      fs.readFile('./test.txt', { encoding: 'utf-8' }, (err, original) => {
+        fs.readFile('./test-copy.txt', { encoding: 'utf-8' }, (err, copy) => {
           expect(copy).toEqual(original);
+          done();
         });
       });
     });
