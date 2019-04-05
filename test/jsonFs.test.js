a114b173f4c32a30b4b28b42c70b6ae74e36c74b
Laura Badgley <l.badgleyy@gmail.com>
2019-04-04 21:04:45 -0700

    read and write JSON tests failing... trying to fix

--- test/jsonFs.test.js
+++ test/jsonFs.test.js
@@ -1,18 +1,41 @@
-const readJSON = require('../lib/jsonFS');
 const fs = require('fs');
+const { 
+  readJSON,
+  writeJSON
+} = require('../lib/writeFile');
 
 describe('jsonFs', () => {
+  // afterEach(done => {
+  //   fs.unlink('../lib/jsonFS', done);
+  // });
+
   it('can read a json file', done => {
     const guy = {
       name: 'bro',
       age: 15
     };
-    fs.writeJSON('./lib/myJson', JSON.stringify(guy), () => {
-      readJSON('./lib/myJSON', (err, data) => {
+    fs.writeFile('./lib/jsonFS', JSON.stringify(guy), () => {
+      readJSON('./lib/jsonFS', (err, data) => {
         expect(err).toBeFalsy();
         expect(data).toEqual(guy);
         done();
       });
     });
   });
+
+  it('can write a json file', done => {
+    const guy = {
+      name: 'bro',
+      age: 15
+    };
+    writeJSON('../lib/jsonFS', guy, err => {
+      expect(err).toBeFalsy();
+
+      readJSON('../lib/jsonFS', (err, data) => {
+        expect(data).toEqual(guy);
+        done();
+      });
+    });
+  });
 });
+
