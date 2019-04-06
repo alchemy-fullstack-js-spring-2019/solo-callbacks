8f8b662e24bece29e255501a295e5caabf46c41c
Emily Creamer <emily.creamer@icloud.com>
2019-04-06 10:36:08 -0700

    trying to fix travis...

--- test/wait.test.js
+++ test/wait.test.js
@@ -7,11 +7,3 @@ describe('wait', () => {
     });
   });
 });
-
-// same, but refactored: 
-
-// describe('wait', () => {
-//   it('delay before the callback', done => {
-//     wait(1000, done);
-//   });
-// });
