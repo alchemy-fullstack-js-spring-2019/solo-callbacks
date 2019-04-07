178824acb8a79688e9c4512bf8a683db20855f18
Payton Lower <paytown@me.com>
2019-04-07 16:08:44 -0700

    copy tests fixed

--- test/copy.test.js
+++ test/copy.test.js
@@ -1,16 +1,17 @@
 const copy = require('../lib/copy');
+const fs = require('fs');
 
 describe('copy a file', () => {
   it('copies a file', done => {
-    let src = null;
-    let dest = null;
-
     copy('./1_callbacks.md', './1_callbacks-copy.md', err => {
       expect(err).toBeFalsy();
 
-      expect(copy(src)).toEqual(copy(dest));
+      fs.readFile('./1_callbacks.md', 'utf8', (err, src) => {
+        fs.readFile('./1_callbacks-copy.md', 'utf8', (err, dest) => {
+          expect(src).toEqual(dest);
+          done();
+        });
+      });
     });
-
-    done();
   });
 });
