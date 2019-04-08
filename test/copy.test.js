2f958c977537e38a896341b2920c0c49e9724b0f
heather <unphurlng@gmail.com>
2019-04-08 05:19:17 -0700

    added afterEach to copy.test.js

--- test/copy.test.js
+++ test/copy.test.js
@@ -2,6 +2,10 @@ const copy = require('../lib/copy');
 const fs = require('fs');
 
 describe('Copy function', () => {
+  afterEach(done => {
+    fs.unlink('./1_callbacks-copy.md', done);
+  });
+  
   it('Copies a file', done => {
     copy('./1_callbacks.md', './1_callbacks-copy.md', err => {
       expect(err).toBeUndefined();
